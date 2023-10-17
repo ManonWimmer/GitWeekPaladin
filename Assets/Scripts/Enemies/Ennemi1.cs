58c286542943a6fb1f720cd9ac584a61c23faa77
LucLenne <luc.lenne@edu.devinci.fr>
2023-10-17 11:06:01 +0200

    tag ennemi

--- Assets/Scripts/Enemies/Ennemi1.cs
+++ Assets/Scripts/Enemies/Ennemi1.cs
@@ -7,17 +7,20 @@ public class Ennemi1 : MonoBehaviour
 {
     [SerializeField] Transform crystal;
     [SerializeField] int _moveSpeed;
+    [SerializeField] Collider2D zoneAttack;
+    [SerializeField] int vieCrystal;
     private void FixedUpdate()
     {
-        if (crystal != null)
-        {
-            Vector3 direction = crystal.position - transform.position;
-            direction.Normalize();
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-            transform.rotation = rotation;
-            transform.position += direction * _moveSpeed * Time.fixedDeltaTime;
-        }
+        Vector3 direction = crystal.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = rotation;
+        transform.position += direction * _moveSpeed * Time.fixedDeltaTime;
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        vieCrystal -= 1;
     }
 }
 
