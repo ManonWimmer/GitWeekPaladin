178b295db8a9542b63b6ebaba6cc66c6a9734dbd
LucLenne <luc.lenne@edu.devinci.fr>
2023-10-17 11:15:43 +0200

    rename tag + variable

--- Assets/Scripts/Enemies/Enemy1.cs
+++ Assets/Scripts/Enemies/Enemy1.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Windows;
 
-public class Ennemi1 : MonoBehaviour
+public class Enemy1 : MonoBehaviour
 {
-    [SerializeField] Transform crystal;
+    [SerializeField] Transform _crystal;
     [SerializeField] int _moveSpeed;
-    [SerializeField] Collider2D zoneAttack;
-    [SerializeField] int vieCrystal;
+    [SerializeField] Collider2D _attackTrigger;
+    [SerializeField] int _lifeCrystal;
     private void FixedUpdate()
     {
-        Vector3 direction = crystal.position - transform.position;
+        Vector3 direction = _crystal.position - transform.position;
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
@@ -20,7 +20,7 @@ public class Ennemi1 : MonoBehaviour
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
-        vieCrystal -= 1;
+        _lifeCrystal -= 1;
     }
 }
 
