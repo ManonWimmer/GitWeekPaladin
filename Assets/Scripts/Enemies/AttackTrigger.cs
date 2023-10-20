62f1415d2dcd2d7e7aa336c8e3ea2a8fd6981c8f
LucLenne <113776725+LucLenne@users.noreply.github.com>
2023-10-20 14:44:51 +0200

    Prefab Enemies

--- Assets/Scripts/Enemies/AttackTrigger.cs
+++ Assets/Scripts/Enemies/AttackTrigger.cs
@@ -7,20 +7,21 @@ public class AttackTrigger : MonoBehaviour
 {
     [SerializeField] MovementEnemy enemy1;
     public bool isMoving;
-    [SerializeField] int timeBeforeAttack;
+    [SerializeField] int _timeBeforeAttack;
+    [SerializeField] int _enemyForce=1;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Crystal"))
         {
             StartCoroutine(CoroutineAttack());
-            LifeCrystal.Instance.TakeDamage(1);
+            LifeCrystal.Instance.TakeDamage(_enemyForce);
             isMoving = false;
         }
     }
 
     IEnumerator CoroutineAttack()
     {
-        yield return new WaitForSeconds(timeBeforeAttack);
+        yield return new WaitForSeconds(_timeBeforeAttack);
         LifeCrystal.Instance.TakeDamage(1);
         StartCoroutine(CoroutineAttack());
     }
